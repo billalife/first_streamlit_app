704e64a7d488bcd2e5c239a3fc7a7325c91eef72
billalife <100465556+billalife@users.noreply.github.com>
2023-01-03 15:51:30 +0530

    Update streamlit_app.py
    added text to the web page

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
 
+st.text('Omega 3 & blueberry Oat meals')
+
+st.text('Kale, Spinach & Rocket Smoothie)
+        
+st.text('Hard-Boiled and Free Range Eggs')        
