0253c494e57df94a4d5b268879d89f8aeddbd699
billalife <100465556+billalife@users.noreply.github.com>
2023-01-03 15:52:42 +0530

    Update streamlit_app.py
    Forgot ' at line 9

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.header('Breakfast Menu')
 
 st.text('Omega 3 & blueberry Oat meals')
 
-st.text('Kale, Spinach & Rocket Smoothie)
+st.text('Kale, Spinach & Rocket Smoothie')
         
 st.text('Hard-Boiled and Free Range Eggs')        
