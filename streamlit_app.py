68d5fbb809413d6eb02a268fee4673f173a5e4b9
billalife <100465556+billalife@users.noreply.github.com>
2023-01-03 16:27:44 +0530

    Update streamlit_app.py
    changed the streamlit to st as st is the alias

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,4 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.dataframe(my_fruit_list)  ##streamlit library to display it on the page by typin
+st.dataframe(my_fruit_list)  ##streamlit library to display it on the page by typin
