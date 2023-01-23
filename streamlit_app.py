3cda91d392c51551b7feba414f602961eddc135b
billalife <100465556+billalife@users.noreply.github.com>
2023-01-23 12:25:12 +0530

    checking if the connection works between streamlit and snowflake works

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 #Display a dataframe as an interactive table.
 st.dataframe(fruityvice_normalized)
+
+#sending message from snowflake i.e checking if the connection works
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
+
