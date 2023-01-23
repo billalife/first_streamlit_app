d5c56c812502e14407822480ddc73e7e1c070691
billalife <100465556+billalife@users.noreply.github.com>
2023-01-23 12:27:54 +0530

    changed to st

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
 
 #sending message from snowflake i.e checking if the connection works
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
