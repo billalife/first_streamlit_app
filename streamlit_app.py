7b62a68cbd5c52a2625eef938ff9809cca3eee65
billalife <100465556+billalife@users.noreply.github.com>
2023-01-29 20:57:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,14 +112,14 @@ if st.button('Get Fruit load list'):
 #Adding a new function for the final block to select fruits
 def insert_row_nowflake(new_fruit):
         with my_cnx.cursor() as my_cur:
-                my_cur.execute( "insert into pc_rivery_db.public.fruit_load_list values ('from st')")
+                my_cur.execute( "insert into pc_rivery_db.public.fruit_load_list values ('back_from_function')")
                 return "Thanks for adding " + new_fruit
 add_my_fruit = st.text_input('What fruit would you like add?')
 if st.button('Add a fruit to the list'):
         my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
         back_from_function = insert_row_nowflake(fruit_choice)
         st.text(back_from_function)
-        st.write("Thanks for adding ", insert_row_nowflake)       
+        st.write("Thanks for adding ", back_from_function)       
         
                 
         
