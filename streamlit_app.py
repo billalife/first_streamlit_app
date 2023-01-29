cd07bb7f001e5356ce49b9ce9cff6108789f9445
billalife <100465556+billalife@users.noreply.github.com>
2023-01-30 01:18:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ st.dataframe(fruits_to_show)  #Display a dataframe as an interactive table.
 
 #MAKING A FUNCTION which move several lines of code into a little group of code called a function, repeatable block of code (function)
 def get_fruityvice_data(this_fruit_choice):
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all" + this_fruit_choice) 
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) 
         fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
         return (fruityvice_normalized)
 st.header("Fruityvice Fruit Advice!")
