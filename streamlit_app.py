f56f8c43941a2ef34b8d84c42b12a656cdb1c6e1
billalife <100465556+billalife@users.noreply.github.com>
2023-01-29 20:41:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-import snowflake.connector 
+import snowflake.connector as sc
 from urllib.error import URLError
 
 st.title('My Mom\'s New Healthy Diner') #special characters handling are to done with '\'
@@ -93,12 +93,12 @@ except URLError as e:
 st.header("The fruit load list contains:")
 #Snowflake-related function
 def get_fruit_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("SELECT * from fruit_load_list")
-        retrun my_cur.fetchall()  #fetches all records from fruit_load_list table
+        with my_cnx.cursor() as my_cur:
+                my_cur.execute("SELECT * from fruit_load_list")
+                retrun my_cur.fetchall()  #fetches all records from fruit_load_list table
 #add a button to load the fruit                
 if st.button('Get Fruit load list'):
-        my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+        my_cnx = sc.connect(**st.secrets["snowflake"])
         my_data_rows = get_fruit_list()
         st.dataframe(my_data_rows)
 except URLError as e:
